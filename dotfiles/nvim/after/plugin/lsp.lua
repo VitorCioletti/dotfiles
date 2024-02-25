39c8897d8a199ef32675c2fe994d56388319f92d
VitorCioletti <ciolettiv@gmail.com>
2024-02-25 13:50:59 -0300

    Configured auto complete.

--- dotfiles/nvim/after/plugin/lsp.lua
+++ dotfiles/nvim/after/plugin/lsp.lua
@@ -2,23 +2,50 @@ local mason_lsp_config = require('mason-lspconfig')
 local mason = require('mason')
 local lsp = require('lsp-zero')
 local lsp_config = require('lspconfig')
+local cmp = require('cmp')
 
 mason.setup {}
 
 mason_lsp_config.setup {
-	ensure_installed = {
-		'omnisharp_mono',
-		'omnisharp',
-		'lua_ls',
-	},
-	automatic_installation = true,
+    ensure_installed = {
+        'omnisharp_mono',
+        'omnisharp',
+        'lua_ls',
+    },
+    automatic_installation = true,
 }
 
-lsp.preset('recommended')
+local cmp_action = lsp.cmp_action()
 
-lsp.setup()
+cmp.setup({
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<tab>'] = cmp.mapping.confirm({select = true}),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    })
+})
 
-lsp_config.lua_ls.setup {}
-lsp_config.omnisharp_mono.setup {}
+lsp.setup {}
+
+local lua_opts = lsp.nvim_lua_ls()
+
+lsp_config.lua_ls.setup {
+    lua_opts,
+    settings = {
+        Lua = {
+            -- Fix Undefined global 'vim'
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+}
 lsp_config.omnisharp.setup {}
 
--- dotfiles/nvim/lua/mine/packer.lua
+++ dotfiles/nvim/lua/mine/packer.lua
@@ -1,38 +1,46 @@
 local function on_startup(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim',
-	  tag = '0.1.5',
-	  requires = { { 'nvim-lua/plenary.nvim' } }
-  }
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-  use { 'catppuccin/nvim', as = 'catppuccin' }
-use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  requires = {
-    -- LSP Support
-     {'neovim/nvim-lspconfig'},
-     {'williamboman/mason-lspconfig.nvim'},
-     {'williamboman/mason.nvim'},
-     {'williamboman/mason-lspconfig.nvim'},
+    use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use { 'catppuccin/nvim', as = 'catppuccin' }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason-lspconfig.nvim'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
-  }
-}
-use {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+    use("mbbill/undotree")
+    use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
         requires = {
             "nvim-lua/plenary.nvim",
             "MunifTanjim/nui.nvim"
         }
-  }
+    }
 end
 
 return require('packer').startup(on_startup)
