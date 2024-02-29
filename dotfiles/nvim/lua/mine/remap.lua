ed61659e428aea4e61254d4d922269cd6b50e8c7
VitorCioletti <ciolettiv@gmail.com>
2024-02-28 23:56:44 -0300

    Added mapping file to so in neovim.

--- dotfiles/nvim/lua/mine/remap.lua
+++ dotfiles/nvim/lua/mine/remap.lua
@@ -1,15 +1,14 @@
 -- clear highlights
 vim.keymap.set('n', '<CR>', ':noh<CR>')
 
--- telescope
 vim.keymap.set('n', '<leader>sf', ':Telescope find_files<CR>')
 vim.keymap.set('n', '<leader>ss', ':Telescope grep_string<CR>')
 
--- neotree
 vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal<CR>')
 
---zen mode
 vim.keymap.set('n', '<leader>zm', ':ZenMode<CR>')
+
+vim.keymap.set('n', '<leader><leader>', ':so<CR>')
 
 -- lsp
 local lsp = require('lsp-zero')
